23b8281876d5258ea3591a00564e23509f370a49
b5710546232 <nattapat.su@ku.th>
2020-10-13 18:14:15 +0700

    :recycle: extract check function and dataset

---
+++ utils/checker.ts
@@ -0,0 +1,18 @@
+import { Dataset } from '../dataset/interface'
+
+export function checker(dataset: Dataset[], input: string) {
+  const out = []
+
+  dataset.forEach((data: Dataset) => {
+    data.words.forEach((word) => {
+      if (input.includes(word)) {
+        out.push({
+          word,
+          type: data.type,
+        })
+      }
+    })
+  })
+
+  return out
+}
