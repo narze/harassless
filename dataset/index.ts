31df265dbf41e323fae194a0b7abb8b57622e082
b5710546232 <nattapat.su@ku.th>
2020-10-13 22:01:03 +0700

    :recycle: import checker to app

---
+++ dataset/index.ts
@@ -0,0 +1,11 @@
+const dataset = [
+  {
+    type: 'Sexual Harassment',
+    words: ['ห อ ม', 'คือลือ', 'ข อ บ คุ ณ ค รั บ', 'คุ ณ ภ า พ'],
+  },
+  {
+    type: 'Ableist Language',
+    words: ['ปสด', 'ประสาทแดก'],
+  },
+]
+export default dataset
